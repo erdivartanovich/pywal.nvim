74ade9c45ed23bff280e7fce3a0141d15419b7ab
erdivartanovich <erdivartanovich@gmail.com>
2023-10-22 10:54:01 +0800

    default disable watch pywal changes

--- lua/pywal/init.lua
+++ lua/pywal/init.lua
@@ -1,19 +1,18 @@
 local M = {}
 local core = require('pywal.core')
 local highlights = require('pywal.highlights')
-local wal_file = os.getenv("HOME") .. "/.cache/wal/colors-wal.vim"
 
 local load_colors = function()
-  print("File changed!", wal_file)
+  vim.opt.termguicolors = true
   local colors = core.get_colors()
   highlights.highlight_all(colors)
 end
 
 function M.setup()
-  vim.opt.termguicolors = true
   load_colors()
-  local watchfile = require('pywal.watchfile')
-  watchfile.watch(wal_file, load_colors)
+  --local wal_file = os.getenv("HOME") .. "/.cache/wal/colors-wal.vim"
+  -- local watchfile = require('pywal.watchfile')
+  -- watchfile.watch(wal_file, load_colors)
 end
 
 return M
