c0832d7235c2e3498f5efc1f2bd060d433fa8f29
erdivartanovich <erdivartanovich@gmail.com>
2025-12-11 14:40:59 +0800

    simplify colors

--- lua/pywal/core.lua
+++ lua/pywal/core.lua
@@ -5,8 +5,8 @@ function M.get_colors()
 
 	return {
 		background = vim.g.background,
-		foreground = vim.g.foreground,
-		cursor = vim.g.color6,
+		foreground = vim.g.color7,
+		cursor = vim.g.cursor,
 		color0 = vim.g.color0,
 		color1 = vim.g.color1,
 		color2 = vim.g.color2,
@@ -15,14 +15,7 @@ function M.get_colors()
 		color5 = vim.g.color5,
 		color6 = vim.g.color6,
 		color7 = vim.g.color7,
-		color8 = vim.g.color8,
-		color9 = vim.g.color9,
-		color10 = vim.g.color10,
-		color11 = vim.g.color11,
-		color12 = vim.g.color12,
-		color13 = vim.g.color13,
-		color14 = vim.g.color14,
-		color15 = vim.g.color15,
+		error = "#C81E1E",
 	}
 end
 
